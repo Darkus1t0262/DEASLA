31f721b8f5fcd4d2393328339c280e793b29128a
Darkus1t0262 <98562981+Darkus1t0262@users.noreply.github.com>
2025-06-17 13:31:48 -0500

    feat: :sparkles: Creating Files for schedule service
    Creating pom.xml, controller and schedule service on javaa

---
+++ domains/alert-managment/schedule-service/src/main/java/com/deasla/schedule/controller/ScheduleServiceController.java
@@ -0,0 +1,14 @@
+package com.deasla.schedule.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+public class ScheduleServiceController {
+
+    @GetMapping("/api/schedules")
+    public List<String> getAll() {
+        return List.of("ScheduleService A", "ScheduleService B");
+    }
+}

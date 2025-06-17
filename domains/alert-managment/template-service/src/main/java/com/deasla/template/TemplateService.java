c09730c3a8628c3e8ea84e97a1dab5ee37fc43bb
Darkus1t0262 <98562981+Darkus1t0262@users.noreply.github.com>
2025-06-17 14:21:37 -0500

    feat: :sparkles: Creating template service
    Creating pom, dockerfile and service controller for this part

---
+++ domains/alert-managment/template-service/src/main/java/com/deasla/template/TemplateService.java
@@ -0,0 +1,11 @@
+package com.deasla.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateService {
+    public static void main(String[] args) {
+        SpringApplication.run(TemplateService.class, args);
+    }
+}

fb027f827713e0e0a24ccc995fabff4c2a0c6dae
Darkus1t0262 <98562981+Darkus1t0262@users.noreply.github.com>
2025-06-26 21:48:34 -0500

    fix: :bug: Fixing problems on paths
    Fixing mistake on go.mod file missing part of the name

---
+++ domains/notification/notification-core/internal/service/core.go
@@ -0,0 +1,16 @@
+package service
+
+import (
+	"context"
+	"notification-core/internal/db"
+)
+
+func SendNotification(recipient, content string) error {
+	// Save notification in the database
+	_, err := db.PgPool.Exec(
+		context.Background(),
+		"INSERT INTO notifications (recipient, content) VALUES ($1, $2)",
+		recipient, content,
+	)
+	return err
+}
